3ca779135b6706fd4f6026ea651673737d2843cf
Lars <larssieboy18@users.noreply.github.com>
2022-09-09 15:32:49 +0200

    Bug fixes

--- helpers/channels/permissions/functions.js
+++ helpers/channels/permissions/functions.js
@@ -2,7 +2,7 @@ const lib = require('lib')({ token: process.env.STDLIB_SECRET_TOKEN });
 module.exports = {
   update: async (channel_id, overwrite_id, role = true, allow, deny) => {
     let type = 0
-    if (!type) { type = 1 }
+    if (!role) { type = 1 }
     return await lib.discord.channels['@0.3.2'].permissions.update({
       overwrite_id: overwrite_id,
       channel_id: channel_id,
