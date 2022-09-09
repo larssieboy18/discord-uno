0780187b4f470231f106af42d9998a00980d08ce
Lars <larssieboy18@users.noreply.github.com>
2022-09-09 12:19:18 +0200

    Update info.js
    Added info.js so it sends an embed to the user with information about the bot, like the contributors, where the cards have been downloaded from and a license notice

--- functions/events/discord/command/info.js
+++ functions/events/discord/command/info.js
@@ -1,11 +1,58 @@
-// authenticates you with the API standard library
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+/* This is importing the responses.js file from the helpers/interactions folder. */
+const responses = require('../../../../helpers/interactions/responses.js');
 
-/*
-Info about the bot, include at least:
-- the creator
-- (if applicable) any contributors
-- a copyright notice
-- the fact where all UNO cards are from https://commons.wikimedia.org/wiki/File:UNO_cards_deck.svg
-*/
+// ACK the event
+await responses.create(context.params.event.token);
 
+/* Creating an embed object that will be sent to the user. */
+let infoEmbed = {
+  title: `UNO Bot`,
+  description: `This bot was created by Lars.#0018 (<@119473151913623552>). If you have any questions, suggestions or found any bugs, please contact me on Discord.`,
+  color: 0x00ff00,
+  fields: [
+    {
+      name: `Contributors`,
+      value: `[Click to view](https://github.com/larssieboy18/discord-uno/graphs/contributors)`,
+      inline: false,
+    },
+    {
+      name: `UNO Cards`,
+      value: `All cards have been downloaded from [Wikipedia](https://en.wikipedia.org/wiki/Uno_(card_game)#/media/File:UNO_cards_deck.svg), which were provided under the [CC0 license](http://creativecommons.org/publicdomain/zero/1.0/deed.en). Any edits have been made by a contributor to the project.`,
+      inline: true,
+    },
+    {
+      name: `License`,
+      value: `This bot is Open Source under the [MPL-2.0 license](https://github.com/larssieboy18/discord-uno/blob/main/LICENSE). You can read more about the license [here](https://choosealicense.com/licenses/mpl-2.0/).`,
+      inline: true,
+    },
+    {
+      name: `Source Code`,
+      value: `View on [GitHub](https://github.com/larssieboy18/discord-uno)`,
+      inline: true,
+    },
+    {
+      name: `Invite`,
+      value: `Invite the bot to your server [here](https://discord.com/api/oauth2/authorize?client_id=${process.env.botID}&permissions=532844899408&scope=applications.commands%20bot).`,
+      inline: true,
+    },
+    {
+      name: `Donate`,
+      value: `If you want to support the development of the UNO bot, you can donate [here](https://opencollective.com/uno-discord-bot).`,
+      inline: true,
+    },
+    {
+      name: `Copyright notice`,
+      value: `© 2022 [larssieboy18](https://github.com/larssieboy18) & [Contributors](https://github.com/larssieboy18/discord-uno/graphs/contributors)`,
+      inline: false,
+    },
+  ],
+};
+
+/* This is sending the embed object to the user. */
+await responses.update(
+  `${context.params.event.token}`,
+  '',
+  [infoEmbed],
+  [],
+  'CHANNEL_MESSAGE_WITH_SOURCE',
+);
