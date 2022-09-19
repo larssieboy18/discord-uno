b90090125f57b08ffbf8366be65f1179c245fdaf
Lars <larssieboy18@users.noreply.github.com>
2022-09-19 23:50:13 +0200

    Update start.js
    Made some changes to the starting message and the player order

--- functions/events/discord/uno/start.js
+++ functions/events/discord/uno/start.js
@@ -16,6 +16,12 @@ try {
           },
           {
             id: `793188304764928023`,
+          },
+          {
+            id: `975790862539825164`,
+          },
+          {
+            id: `976400332299063366`,
           }
         ]
       },
@@ -56,6 +62,7 @@ try {
   //     hand: hand
   //   }
   // })
+
   for (let i = 0; i < playerlist.length; i++) {
     playerHands[i] = startingDeck.slice(0, 7);
   }
@@ -69,15 +76,20 @@ try {
   // pick a random player from playerlist that starts the game
   let startingPlayer = playerlist[Math.floor(Math.random() * playerlist.length)];
 
+  // create a list of players in the order that they will go
+  let playerOrder = playerlist.slice(playerlist.indexOf(startingPlayer)).concat(playerlist.slice(0, playerlist.indexOf(startingPlayer)));
+  console.log(playerOrder)
+
   // create game object
   let game = {
     channel: gameChannel, // channel where the game is being played
     playerlist: playerlist, // array of player objects
     playerHands: playerHands, // object with player ids as keys and arrays of cards as values
+    playerOrder: playerOrder, // array of player objects in the order that they will go
     discardPile: discardPile, // discard pile has one card at the start of the game
     drawPile: startingDeck, // draw pile is the remaining cards after the first card has been added to the discard pile and player hands have been dealt
     currentPlayer: startingPlayer, // player object of the player that starts the game
-    direction: 1, // reverse
+    direction: 1, // reverse (⬅️ & ➡️) OR (⬆️ & ⬇️) : 1 = normal, -1 = reverse
     drawTwo: 0, // +2
     skip: 0, // skip
     wildColor: null, // color change because of wildcard
@@ -89,8 +101,8 @@ try {
     unoMissedCalledBy: null, // player who noticed that someone missed calling uno
   };
 
-  // debugging
-  console.log(game);
+  // // debugging
+  // console.log(game);
   // console.log(game.playerHands);
 
   // save game to kv
@@ -109,15 +121,41 @@ try {
       color: 0x00ff00,
       fields: [
         {
-          name: "Current Card",
-          value: `${(await getCardByName(discardPile[0])).emoji} ${(await getCardByName(discardPile[0])).name.replace(/\_/gi, ` `)}`,
-          //value: `${discardPile[0]}`,
-          inline: true,
-        },
-        {
           name: "Current Player",
           value: `<@${startingPlayer.id}>`,
           inline: true,
+        },
+        // player order including the direction
+        {
+          name: "Player Order",
+          value: `${playerOrder.map(player => `<@${player.id}>`).join(`\n`)}`,
+          inline: true,
+        },
+        {
+          name: "Direction",
+          value: `${game.direction === 1 ? `⬇️` : `⬆️`}`,
+          inline: true,
+        },
+        /* Creating a field with the name "Player Hands" and show how many cards every player has left */
+        {
+          name: "Player Hands",
+          value: `${playerlist.map(player => `<@${player.id}>: ${playerHands[playerlist.indexOf(player)].length} cards`).join(`\n`)}`,
+          inline: true,
+        },
+        {
+          name: "Discard Pile",
+          value: `${discardPile.length} cards`,
+          inline: true,
+        },
+        {
+          name: "Draw Pile",
+          value: `${startingDeck.length} cards`,
+          inline: true,
+        },
+        {
+          name: "Current Card",
+          value: `${await getCardByName(discardPile[0]).emoji} ${await getCardByName(discardPile[0]).name}`,
+          inline: false,
         },
       ],
       image: {
@@ -126,6 +164,12 @@ try {
       },
     },
   ], []);
+
+  // TODO Change description of embed when it is not the start of the game anymore
+  // lines 120
+  // labels: development
+  // assignees: larssieboy18
+  // Currently, the message shows a message saying that the player that starts the game starts the game. For consequent turns, it should be altered to show that it is someones turn instead of the person starting a game.
 
   // debugging
   console.log(startingMessage)
