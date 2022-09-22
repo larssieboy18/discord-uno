51ea48aa00c8b043e9a98cb2cfcb4826c41ccbaf
Lars <larssieboy18@users.noreply.github.com>
2022-09-22 20:23:02 +0200

    Update turn.js

--- functions/events/discord/uno/turn.js
+++ functions/events/discord/uno/turn.js
@@ -1,4 +1,17 @@
 try {
+  const uno_deck = require('../../../../helpers/others/uno_deck.json');
+  const { unoCards } = require('../../../../helpers/others/uno_cards.json');
+  const { shuffle, getCardByName } = require('../../../../helpers/others/functions.js');
+  const kv = require('../../../../helpers/kv/functions.js');
+  const messages = require('../../../../helpers/channels/messages/functions.js');
+
+  let { guild_id } = context.params.event
+  let gameChannel = context.params.event.channel_id
+
+  // Get current game data
+  await kv.get(`UNO-${guild_id}-${gameChannel}`);
+
+  
 
 
   // did an error occur?
